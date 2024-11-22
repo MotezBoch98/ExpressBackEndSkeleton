1855378fc82b7c1567be44776735ca6f5b0673ea
MotezBoch98 <89611046+MotezBoch98@users.noreply.github.com>
2024-11-22 13:08:58 +0100

    changed to jwt instead of saving tokens in db for login

--- src/models/EmailVerificationToken.js
+++
@@ -1,9 +0,0 @@
-import mongoose from 'mongoose';
-
-const emailVerificationTokenSchema = new mongoose.Schema({
-    userId: { type: mongoose.Schema.Types.ObjectId, required: true, ref: 'User' },
-    token: { type: String, required: true },
-    createdAt: { type: Date, default: Date.now, expires: 3600 }, // Expires after 1 hour
-});
-
-export default mongoose.model('EmailVerificationToken', emailVerificationTokenSchema);
--- src/models/PasswordResetToken.js
+++
@@ -1,9 +0,0 @@
-import mongoose from 'mongoose';
-
-const passwordResetTokenSchema = new mongoose.Schema({
-    userId: { type: mongoose.Schema.Types.ObjectId, required: true, ref: 'User' },
-    token: { type: String, required: true },
-    createdAt: { type: Date, default: Date.now, expires: 3600 }, // Expires in 1 hour
-});
-
-export default mongoose.model('PasswordResetToken', passwordResetTokenSchema);
