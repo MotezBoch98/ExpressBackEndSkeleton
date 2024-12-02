d333263c47d171e920b6baf12332628dafbd5406
MotezBoch98 <89611046+MotezBoch98@users.noreply.github.com>
2024-12-02 23:00:24 +0100

    added cors for cross origin issues

--- src/app.js
+++ src/app.js
@@ -1,20 +1,30 @@
 import connectDB from './config/db.js';
 import authRoutes from './routes/authRoutes.js';
+import { swaggerSpec, swaggerUi } from './config/swagger.js';
+import errorHandler from './middlewares/errorHandler.js';
 import express from 'express';
 import dotenv from 'dotenv';
-import { swaggerSpec, swaggerUi } from './config/swagger.js'; // Import Swagger configuration
+import cors from 'cors';
 
 dotenv.config();
 connectDB();
 
 const app = express();
 
+app.use(cors({
+    origin: 'http://localhost:5000',
+    credentials: true
+}));
+
 app.get('/swagger.json', (req, res) => {
     res.json(swaggerSpec);
 });
 
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec)); // Swagger setup
 app.use(express.json());
+app.use(errorHandler);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec)); // Swagger setup
 app.use('/api/auth', authRoutes);
+
+console.log('App initialized and routes set up');
 
 export default app;
