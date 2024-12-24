5665d5e2c7504f0ccfa5c4da1a8e5c7ac7efd494
MotezBoch98 <89611046+MotezBoch98@users.noreply.github.com>
2024-12-25 00:50:46 +0100

    Documentation+

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -12,6 +12,14 @@ import * as authService from '../services/authService.js';
  * @param {Object} res - The response object.
  * @returns {Promise<void>}
  */
+export const register = async (req, res) => {
+    try {
+        const user = await authService.registerUser(req.body);
+        res.status(201).json({ success: true, data: user });
+    } catch (error) {
+        res.status(400).json({ success: false, message: error.message });
+    }
+};
 
 /**
  * Logs in a user.
