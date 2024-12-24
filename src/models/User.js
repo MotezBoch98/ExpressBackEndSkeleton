00863709d93268e78583e7e4659853872eecb0f8
MotezBoch98 <89611046+MotezBoch98@users.noreply.github.com>
2024-12-25 00:37:11 +0100

    documentation+

--- src/models/User.js
+++ src/models/User.js
@@ -1,5 +1,16 @@
 import mongoose from 'mongoose';
 
+/**
+ * User Schema
+ * 
+ * @typedef {Object} User
+ * @property {string} name - The name of the user.
+ * @property {string} email - The email of the user.
+ * @property {string} password - The password of the user.
+ * @property {boolean} isVerified - Indicates if the user's email is verified.
+ * @property {Date} createdAt - The date when the user was created.
+ * @property {Date} updatedAt - The date when the user was last updated.
+ */
 const userSchema = new mongoose.Schema(
     {
         name: { type: String, required: true },
